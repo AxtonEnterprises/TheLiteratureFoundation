79c6a7262d9511355d780f6ccc38ae3fabefc04d
Axton <81790772+AxtonEnterprises@users.noreply.github.com>
2026-08-27 14:02:36 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Login from "./pages/Login";
 import Privacy from "./pages/Privacy.jsx";
 import Terms from "./pages/Terms.jsx";
 import Profile from "./pages/Profile";
-import Margins from "./pages/Margins";
+import Chain from "./pages/Chain";
 import NotFound from "./pages/NotFound.jsx";
 import PublicProfile from "./pages/PublicProfile";
 import Group from "./pages/Group";
@@ -54,8 +54,7 @@ export default function App() {
           <Route path="/read/about" element={<About />} />
           <Route path="/read/login" element={<Login />} />
 
-          <Route path="/read/chain" element={<Margins />} />
-          <Route path="/read/margins" element={<Navigate to="/read/chain" replace />} />
+          <Route path="/read/chain" element={<Chain />} />
 
           {/* Compatibility redirects for the former standalone Random Reads routes. */}
           <Route path="/search" element={<Navigate to="/read/search" replace />} />
